9b1713e4799203241324d8fe90297e491cf10d87
Edmund-Norbert <edmundnorbert@gmail.com>
2022-06-02 22:41:17 -0700

    Hello print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+	        puts("\"Programming is like building a multilingual puzzle");
+
+		        return (0);
+
+}
+
+root@29cb72c3ab23:/Norbert/alx-low_level_programming/0x00-hello_world# cat 5-printf.c
+
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+	        printf("with proper grammar, but the outcome is a piece of art,\n");
+
+		        return (0);
+
+}
+
+
