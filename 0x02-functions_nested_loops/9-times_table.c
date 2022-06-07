800e5da618fcfc03de65e80c0f4cbc090f3c6dd1
Edmund-Norbert <104764393+Edmund-Norbert@users.noreply.github.com>
2022-06-08 00:23:57 +0100

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,12 +1,12 @@
-#include "main.h"
+#include <stdio.h>
 
-/**
-* main - check the code
-*
-* Return: Always 0.
-*/
-int main(void)
+int main() 
 {
-times_table();
-return (0);
+int n, i;
+printf("Enter an integer: ");
+scanf("%d", &n);
+for (i = 1; i <= 10; ++i) {
+printf("%d * %d = %d \n", n, i, n * i);
+}
+return 0;
 }
