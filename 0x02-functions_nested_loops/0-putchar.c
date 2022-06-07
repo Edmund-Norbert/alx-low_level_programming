8f34f2e0941ad9ff5dafd89c64ec74c07772cef5
Edmund-Norbert <104764393+Edmund-Norbert@users.noreply.github.com>
2022-06-07 23:31:45 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,10 +2,10 @@
 #include "main.h"
 
 /**
- * main - Prints putchar as a message.
- *
- * Return: Always 0 (Success)
- */
+* main - Prints putchar as a message.
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
