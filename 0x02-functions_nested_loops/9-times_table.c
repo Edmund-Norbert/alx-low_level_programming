594aab05e85c0eb894587d4f405b86b83eebe546
Edmund-Norbert <104764393+Edmund-Norbert@users.noreply.github.com>
2022-06-14 08:49:37 +0100

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _isalpha - Shows 1 if the input is a
- * letter Another cases, shows 0
- *
- * @c: The character in ASCII code
- *
- * Return: 1 for letters. 0 for the rest.
- */
+* _isalpha - Shows 1 if the input is a
+* letter Another cases, shows 0
+*
+* @c: The character in ASCII code
+*
+* Return: 1 for letters. 0 for the rest.
+*/
 int main() 
 {
 int n, i;
