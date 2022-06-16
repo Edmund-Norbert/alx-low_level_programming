9060b9dc82f2a0bc71f752070133c5c74f8baf45
Edmund-Norbert <104764393+Edmund-Norbert@users.noreply.github.com>
2022-06-16 08:00:42 +0100

    Update 103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -27,7 +27,7 @@ int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
         c1--, c2--, size_r--;
         dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
         while (bg >= 0)
-}
+        {
                 op = dr1 + dr2 + add;
                 if (op >= 10)
                         add = op / 10;
