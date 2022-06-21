0db49d7de135f8f3aa6426e7c0228a824d2c6cf8
Edmund-Norbert <104764393+Edmund-Norbert@users.noreply.github.com>
2022-06-21 23:57:00 +0100

    Update 5-main.h

--- 0x07-pointers_arrays_strings/5-main.h
+++ 0x07-pointers_arrays_strings/5-main.h
@@ -1,23 +1,18 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
-* _strpbrk - searches a string for any of a set of bytes.
-* @s: first string.
-* @accept: second string.
-* Return: a pointer to the byte in s that matches one of the
-* bytes in accept, or NULL if no such byte is found.
+* main - check the code for Holberton School students.
+*
+* Return: Always 0.
 */
-char *_strpbrk(char *s, char *accept)
+int main(void)
 {
-unsigned int i, j;
+char *s = "wworld of this";
+char *f = "world";
+char *t;
 
-for (i = 0; *(s + i) != '\0'; i++)
-{
-for (j = 0; *(accept + j) != '\0'; j++)
-{
-if (*(s + i) == *(accept + j))
-return (s + i);
-}
-}
-return ('\0');
+t = _strstr(s, f);
+printf("%s\n", t);
+return (0);
 }
